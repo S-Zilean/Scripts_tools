4fcd85c6fd90a2aeb12e1a69ae313b7440775513
Adil <abouzit@chapsvision.com>
2025-08-05 16:04:23 +0200

    mod: correction datanode_sqlerror.md

--- sdiff.sh
+++ sdiff.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 
-username=$(whoami)
+user=$(whoami | awk'{print $1}')
 
 
 remote_temp="~/.script_tmp"
+dist_dir="/home/${user}/.tempdir"
 repo_dir=$(timeout '0.1' find /* -name 'Scripts_tools' -type d 2>/dev/null)
 repo_tmp="${repo_dir}/tempdir"
 
@@ -18,35 +19,43 @@ echo '--------------------------------------------------------------------------
 echo
 
 read -p "Adresse IP 1 : " ip1
-read -p "Adresse IP 1 : " ip2
+read -p "Adresse IP 2 : " ip2
 
 read -p "Chemin complet du fichier sur ip1 à utiliser : " file1
 read -p "Chemin complet du fichier sur ip2 à utiliser : " file2
 
 # Récupération file1
 getfile1(){
-	ssh -At ${user}@${ip1} "sudo mkdir ${remote_temp} && sudo cp ${file1} ${remote_temp}/file1 "
-	scp ${user}@${ip1}:${remote_temp}/file1 $repo_tmp/.
-	ssh -At ${user}@${ip1} "sudo rm -fr ${remote_temp}"
+
+	ssh -At $ip1 mkdir $dist_dir
+       	ssh -At $ip1 sudo cp ${file1} $dist_dir/file1
+	scp $ip1:${remote_temp}/file1 $repo_tmp/.
+	ssh -At $ip1 rm -fr $dist_dir
+
 }	
 
 # Récupération file2
-getfile1(){
-	ssh -At ${user}@${ip1} "sudo mkdir ${remote_temp} && sudo cp ${file1} ${remote_temp}/file1 "
-	scp ${user}@${ip1}:${remote_temp}/file1 $repo_tmp/.
-	ssh -At ${user}@${ip1} "sudo rm -fr ${remote_temp}"
+getfile2(){
+
+	ssh -At ${ip2} mkdir 
+	ssh -At ${ip2} sudo cp ${file1} /home/$user/.tempdir/file2
+	scp ${ip2}:${remote_temp}/file2 $repo_tmp/.
+	ssh -At ${ip2} rm -fr ${remote_temp}
+
 }	
 
 
 comparefiles(){
+
 	sdiff $repo_tmp/file1 $repo_tmp/file2
+
 }
 
 script_exec=0
-while [ ${script_exec} == 0 ];
+while [ ${script_exec} == 0 ]
 do
 	getfile1
 	getfile2
 	comparefiles
-	$script_exec=1
+	script_exec=1
 done
